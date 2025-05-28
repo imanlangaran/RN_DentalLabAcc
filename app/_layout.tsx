e7683997d99cdc99b6cd550ff9e3e505e2ce17a9
Iman Langaran <73127950+imanlangaran@users.noreply.github.com>
2025-05-28 08:32:53 +0330

    feat: add expo-sqlite-devtools for enhanced SQLite debugging and update package dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,24 +4,28 @@ import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 import { Stack } from "expo-router";
 import { Platform } from "react-native";
+import { useSQLiteDevTools } from 'expo-sqlite-devtools';
 import "../global.css";
 
 
 const LOCAL_DATABASE_NAME = 'RN_DentalLabAcc.db';
 
 let db: any = null;
+let expoDB: any = null;
 
-//shoud be comment when developing ui
+////// shoud be comment when developing ui
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { openDatabaseSync } from "expo-sqlite";
 if (Platform.OS !== "web") {
-  const expoDB = openDatabaseSync(LOCAL_DATABASE_NAME);
+  expoDB = openDatabaseSync(LOCAL_DATABASE_NAME);
   db = drizzle(expoDB);
 }
 
 export default function RootLayout() {
 
   const { success, error } = useMigrations(db, migrations);
+  useSQLiteDevTools(expoDB);
+
 
   console.log('error', error);
   console.log('success', success);
