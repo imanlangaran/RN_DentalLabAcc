188b874f9eeeaf96ea05ef8b02264bcdd4fbb892
Iman Langaran <73127950+imanlangaran@users.noreply.github.com>
2025-05-28 07:56:15 +0330

    refactor: reorganize imports and enhance database initialization in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,30 @@
+import migrations from "@/drizzle/migrations";
 import i18n from "@/lang/i18n";
 import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { Stack } from "expo-router";
-import "../global.css";
-import { Platform } from "react-native";
-import { openDatabaseSync } from "expo-sqlite";
-import { drizzle } from "drizzle-orm/expo-sqlite";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
-import migrations from "@/drizzle/migrations";
+import { Stack } from "expo-router";
+import { Platform } from "react-native";
+import "../global.css";
+
 
 const LOCAL_DATABASE_NAME = 'RN_DentalLabAcc.db';
 
+let db: any = null;
+
+//shoud be comment when developing ui
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { openDatabaseSync } from "expo-sqlite";
 if (Platform.OS !== "web") {
   const expoDB = openDatabaseSync(LOCAL_DATABASE_NAME);
-  const db = drizzle(expoDB);
+  db = drizzle(expoDB);
 }
 
 export default function RootLayout() {
 
-  // if (Platform.OS !== "web") {
-  //   const { success, error } = useMigrations(db, migrations);
-  // }
+  const { success, error } = useMigrations(db, migrations);
+
+  console.log('error', error);
+  console.log('success', success);
 
   return (
     <ThemeProvider value={DefaultTheme}>
