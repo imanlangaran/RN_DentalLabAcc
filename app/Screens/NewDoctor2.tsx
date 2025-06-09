994e34fbe2c17f69df5cceeb08e57dbdd612bd83
Iman Langaran <imanlangaran@gmail.com>
2025-06-09 19:27:41 +0330

    refactor: clean up NewDoctor2 component structure and remove unnecessary styles

--- app/Screens/NewDoctor2.tsx
+++ app/Screens/NewDoctor2.tsx
@@ -1,24 +1,33 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import BottomButton from '@/Components/BottomButton'
+import { buttomButtonStyle } from '@/styles'
 
 const NewDoctor2 = () => {
   return (
     <View className='screen-container'>
-      <View className='flex justify-center px-8 w-full flex-1 bg-red-800'>
+      <View className='flex justify-center w-full flex-1 bg-red-800'>
 
-      {/* the view for buttom button */}
-      <View className='absolute bottom-0 left-0 right-0 h-16 w-full flex items-center bg-[#fff5] '>
-        <View className='h-full w-10/12 bg-blue-300'>
+        {/* container for buttom button */}
+        <View className='absolute bottom-0 left-0 right-0 h-16 w-full flex items-center '>
+          <View 
+          // className='h-full w-10/12 bg-blue-300'
+          style={buttomButtonStyle.button}
+          >
 
+            <BottomButton
+              title={"Save Doctor"}
+              disable={false}
+              disabledText={"Select"}
+              onPress={() => { }}
+              className="mt-8 py-3"
+            />
+          </View>
         </View>
       </View>
-      </View>
-
 
       {/* keyboard view */}
-      <View className='w-full h-2/5'>
-
-      </View>
+      <View className='w-full h-[200px]' />
     </View>
   )
 }
