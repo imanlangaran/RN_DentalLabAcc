50912a5503cf2202d02de7c0df8a09757b654c96
Iman Langaran <73127950+imanlangaran@users.noreply.github.com>
2025-06-09 10:17:46 +0330

    just testing

--- app/Screens/NewDoctor2.tsx
+++ app/Screens/NewDoctor2.tsx
@@ -3,8 +3,22 @@ import React from 'react'
 
 const NewDoctor2 = () => {
   return (
-    <View>
-      <Text>NewDoctor2</Text>
+    <View className='screen-container'>
+      <View className='flex justify-center px-8 w-full flex-1 bg-red-800'>
+
+      {/* the view for buttom button */}
+      <View className='absolute bottom-0 left-0 right-0 h-16 w-full flex items-center bg-[#fff5] '>
+        <View className='h-full w-10/12 bg-blue-300'>
+
+        </View>
+      </View>
+      </View>
+
+
+      {/* keyboard view */}
+      <View className='w-full h-2/5'>
+
+      </View>
     </View>
   )
 }
